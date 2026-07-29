5d46140ac3fb51195011ee4e3b25a2f08b3a908a
Zaid Hatahet <zaid.hatahet.2023@gmail.com>
2026-07-29 10:12:22 +0300

    Update vite.config.js
    Fix MIME type

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/ecommerce-app/',
   publicDir: 'img',
   build: {
-    outDir: 'dist'  // ★ هذا السطر الجديد مهم جداً للنشر على Vercel ★
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
   }
 })
