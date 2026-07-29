6f50596f358026f933c370e00e2a8c6026a5810c
Zaid Hatahet <zaid.hatahet.2023@gmail.com>
2026-07-29 09:58:51 +0300

    Update vite.config.js
    fix build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ecommerce-app/', // هذا موجود عندك
-  publicDir: 'img'         // ★ هذا هو السطر الجديد والمهم جداً ★
+  base: '/ecommerce-app/',
+  publicDir: 'img',
+  build: {
+    outDir: 'dist'  // ★ هذا السطر الجديد مهم جداً للنشر على Vercel ★
+  }
 })
